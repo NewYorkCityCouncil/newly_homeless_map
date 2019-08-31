c5cea1e50ff76aea7649f729979c421969490524
Ben Witte <bwitte@council.nyc.gov>
2019-08-30 20:41:12 -0400

    added population estimates

--- 01_preprocessing_and_eda.R
+++ 01_preprocessing_and_eda.R
@@ -2,6 +2,13 @@ library(sf)
 library(tidyverse)
 library(leaflet)
 library(janitor)
+library(tidycensus)
+
+
+
+# Read and clean community district data ----------------------------------
+
+
 
 last_known_raw <- read_csv('Associated_Address_by_Borough_and_Community_District.csv') %>% 
   clean_names()
@@ -30,6 +37,11 @@ homeless <- left_join(comdist, agg_last_known)
 homeless <- homeless %>% st_transform(., crs = '+proj=longlat +datum=WGS84')
 
 homeless_clean <- filter(homeless, !is.na(cases), !is.na(people))
+
+
+
+# Map raw numeric data ----------------------------------------------------
+
 
 
 pal_cases <- colorBin(
@@ -66,3 +78,57 @@ leaflet(homeless_clean) %>%
                    options = layersControlOptions(collapsed = FALSE),
                    position = 'bottomright')
 
+
+
+# Load puma data ----------------------------------------------------------
+
+v17 <- load_variables(2017, "acs5", cache = TRUE)
+
+puma <- get_acs(geography = 'public use microdata area', 
+                state = "NY", 
+                variables = 'B01003_001', 
+                year = 2017,
+                key = 'd74c1b2b7e13feac45f63d4b5f2e8789338fc6fb') %>% 
+  janitor::clean_names()
+
+#clean name column in two different columns, so it only represents community districts
+puma_nyc <- puma %>% filter(., grepl('NYC', name))
+split_name <- puma_nyc$name
+rexp <- "^(\\w+\\-\\w+)\\s?(.*)$"
+output_names <- data.frame(boro=sub(rexp,"\\1",split_name), cd=sub(rexp,"\\2",split_name))
+output_names$geoid <- puma_nyc$geoid
+puma_nyc <- left_join(puma_nyc, output_names)
+
+puma_nyc$cd <- gsub("[A-Za-z\\,\\;\\ \\.\\'\\-]", '', puma_nyc$cd)
+puma_nyc$cd <- gsub("[//&]", " ", puma_nyc$cd)
+
+#make separate row for each community district, in preparation for join
+puma_nyc <- separate_rows(puma_nyc, cd)
+puma_nyc$cd <- trimws(puma_nyc$cd)
+puma_nyc <- puma_nyc %>% filter(., grepl("[0-9]", cd))
+puma_nyc$cd <- as.numeric(puma_nyc$cd)
+
+
+#reclassify community boards numerically
+puma_nyc$boro <- gsub('NYC-Manhattan', '100', puma_nyc$boro)
+puma_nyc$boro <- gsub('NYC-Brooklyn', '300', puma_nyc$boro)
+puma_nyc$boro <- gsub('NYC-Bronx', '200', puma_nyc$boro)
+puma_nyc$boro <- gsub('NYC-Staten', '500', puma_nyc$boro)
+puma_nyc$boro <- gsub('NYC-Queens', '400', puma_nyc$boro)
+puma_nyc$boro <- as.numeric(puma_nyc$boro)
+
+#combine boro and cd
+puma_nyc$community_district <- puma_nyc$boro+puma_nyc$cd
+
+
+#scrap unnecessary columns
+puma_nyc_final <- puma_nyc %>% select(.,c(geoid, estimate,community_district))
+
+
+
+# Join estimates and homelessness data ------------------------------------
+
+# NOTE: there will be some issues, as certain geoid's have been estimated as the sum total of multiple cd's
+# for this reason, I've left the geoid's in, so we can look to figure out the issue later.
+
+homeless_clean <- left_join(homeless_clean, puma_nyc_final, by = c("boro_cd" = "community_district"))
